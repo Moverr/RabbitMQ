ac1f4dc6b217b508ec5c6e86c6de2de52842d329
Muyinda ROgers <moverr@gmail.com>
2020-04-19 22:11:58 +0300

    Debugging :

---
+++ Helloworld/src/main/java/com/helloworld/Send.java
@@ -0,0 +1,36 @@
+package com.helloworld;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author muyin
+ */
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public void sendNotification() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+
+    }
+}
