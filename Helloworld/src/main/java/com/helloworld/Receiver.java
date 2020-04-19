f0026971ac9a7fdb56fb758bb680c2525386e32b
Muyinda ROgers <moverr@gmail.com>
2020-04-19 23:03:41 +0300

    Using Java 1.8 Support for Lambdas :

--- Helloworld/src/main/java/com/helloworld/Receiver.java
+++ Helloworld/src/main/java/com/helloworld/Receiver.java
@@ -1,9 +1,9 @@
 package com.helloworld;
 
-
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
@@ -12,20 +12,27 @@ import java.util.concurrent.TimeoutException;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author muyin
  */
 public class Receiver {
-    private final static String QUEUE_NAME = "hello";
-    public void receive() throws IOException, TimeoutException{
-          ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("localhost");
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
 
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+    private final static String QUEUE_NAME = "hello";
+
+    public void receive() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
     }
 }
