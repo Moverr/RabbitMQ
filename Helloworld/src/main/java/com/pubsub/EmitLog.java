5919eea8ad32aee01f61a9104fa482d42c470286
Muyinda ROgers <moverr@gmail.com>
2020-05-05 21:12:19 +0300

    :Debugging :

--- Helloworld/src/main/java/com/pubsub/EmitLog.java
+++ Helloworld/src/main/java/com/pubsub/EmitLog.java
@@ -5,12 +5,35 @@
  */
 package com.pubsub;
 
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  *
  * @author muyin
  */
 public class EmitLog {
-    public static void main(String[] args) {
-        
+
+    private static String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel();) {
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+            String message = args.length < 1 ? "info: Hello World!" : String.join(" ", args);
+
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+
+        }
+
     }
 }
